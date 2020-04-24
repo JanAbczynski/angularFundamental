3cd75b31d88fba74c9787169f877a48e6b35d2a4
JanAbczynski <jan.abczynski@gmail.com>
2020-04-24 15:18:19 +0200

    switch class in function

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -9,8 +9,7 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
             <div class="well hoverwell thumbnail">
                 <h2>{{event.name}}</h2>
                 <div>Date: {{event.date}}</div>
-                <div [ngClass]="{green: event?.time === '8:00 am',
-                    bold: event?.time === '8:00 am'}" 
+                <div [ngClass]="getStartTimeClass()"
                     [ngSwitch]="event?.time">
                     Time: {{event.time}}
                     <span *ngSwitchCase="'8:00 am'">Early start</span>
@@ -38,6 +37,21 @@ styles:[`
 export class EventsThumbnailComponent{
     @Input() event:any
     someProperty:any = "some value"
+
+    // version 1
+    // getStartTimeClass(){
+    //     const isEarlyStart = this.event && this.event.time === '8:00 am'
+    //     return {green: isEarlyStart, bold: isEarlyStart}
+    // }
+
+    // version 2
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
+    
+    
 
     logFoo(){
         console.log('foo')
